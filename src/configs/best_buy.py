e1d539567aa4631623bb19348798f2368d94b956
Kent <kenttdang@gmail.com>
2025-11-18 17:43:14 -0500

    fix: 🔧 updated configs

--- src/configs/best_buy.py
+++ src/configs/best_buy.py
@@ -1,3 +1,11 @@
+BEST_BUY_SELECTOR = {
+    "title": "h1.h4",
+    "price": "div[data-testid='price-block-customer-price'] span",
+    "description": "div[data-testid='brix-sheet-content']",
+    "features": "div[data-testid='brix-sheet-content']",  
+    "images": "div.pr-300.flex.flex-col.items-start"
+}
+
 BEST_BUY_PRODUCT_URL_SELECTOR = {
     "home_url": "https://www.bestbuy.com/",
     "search_box": {
--- src/configs/bestbuy.py
+++
@@ -1,7 +0,0 @@
-BESTBUY_SELECTOR = {
-    "title": "h1.h4",
-    "price": "div[data-testid='price-block-customer-price'] span",
-    "description": "div[data-testid='brix-sheet-content']",
-    "features": "div[data-testid='brix-sheet-content']",  
-    "images": "div.pr-300.flex.flex-col.items-start"
-}
