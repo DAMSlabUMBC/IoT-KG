17213e36fd3c4c06242f9b128612669f05952e91
haimgia <giaoriana24@gmail.com>
2026-06-09 09:26:58 -0400

    fixed __init__ files

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,7 +1,9 @@
 from .entity_analysis import EntityAnalysis
 from .iot_classification import IoTClassification
+from .relationship_analysis import TriplesAnalysis
 
 __all__ = [
     'EntityAnalysis',
-    'IoTClassification'
+    'IoTClassification',
+    'TriplesAnalysis'
 ]
